698e5bd1e8f1412790bb55899b6444ace874a90f
Morcourt <victor.ribeiro.96@msn.com>
2019-10-06 14:53:31 -0300

    tela de logine e cadastro pronta

--- app/src/main/java/com/example/unifood/Cadastro.java
+++ app/src/main/java/com/example/unifood/Cadastro.java
@@ -12,6 +12,7 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        getSupportActionBar().hide();
 
 
 
